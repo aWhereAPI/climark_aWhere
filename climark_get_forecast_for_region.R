f8fcc586b19f103decd7dcb870ed99f91db549a1
vscholl <victoria.scholl@colorado.edu>
2018-06-17 12:48:50 -0600

    added mapping section for 3- and 7-day forecasts

--- climark_get_forecast_for_region.R
+++ climark_get_forecast_for_region.R
@@ -1,8 +1,10 @@
 # load required packages 
-library(aWhereAPI)
 library(dplyr)
-#library(tmap)
-#library(sf)
+library(ggmap)
+library(data.table)
+library(tibble)
+library(aWhereAPI)
+library(aWhereCharts)
 
 # get a forecast for a region - 1 to 7 day aggregate
 # read in the region then using the lat, long - pull the forecast 
@@ -40,18 +42,6 @@ day.start <- as.character(Sys.Date()-1)   # yesterday
 day.end <- as.character(Sys.Date()+7) # 7 days from now
 #day.end <- "2018-06-10" # fixed end date
 
-
-# This works! Grabs a nice forecast df
-# forecast2 pulls only the forecast 
-# it seems odd, but forecasts_latlng can only pull forecast but weirdly generateaWheredataset
-# appears to require you grab at least 1 day observed...
-# foredat <- as.character((Sys.Date()))
-# forecast2 <- forecasts_latlng(lat, lon, day_start = foredat, day_end = day_end, block_size = 24)
-# agronomic_values_latlng() 
-# ag2 <- agronomic_values_latlng(24.74628, 68.4232, day_start = "2016-03-15", day_end = "2016-04-01")
-# obs2 <- daily_observed_latlng(latpt, longpt, day_start = day_start, day_end = day_end)
-  
-
 start.time <- Sys.time()    
 
 # loops through all location ID's in the template file
@@ -65,17 +55,14 @@ for (i in 1:nrow(template.place)){
   # clear/reset the forecast data frame
   forecast1 <- data.frame()    
   
-  # pull forecast and LTN for the period year_start to year_end
+  # pull forecast and LTN for the period year.start to year.end
   forecast1 <- generateaWhereDataset(lat = lat, 
                                  lon = lon, 
                                  day_start = day.start, 
                                  day_end = day.end, 
                                  year_start = year.start, 
-                                 year_end = year.end)# %>% 
-#select( -c(accumulatedPrecipitation.amount,gdd,gdd.average,ppet, accumulatedPpet)) %>%
-#  select( -c(minTemp, maxTemp, precip, pet.amount, accumulatedPrecipitation.average)) %>%
-#  select( -c( accumulatedPet.amount, accumulatedPet.average, accumulatedPpet.average))
-
+                                 year_end = year.end)
+  
   forecast1 = forecast1[-1,]  #this removes the first row - which is observed
   forecast1$locationid <- loc.ID
 
@@ -86,7 +73,7 @@ for (i in 1:nrow(template.place)){
     forecast.all <- rbind(forecast1, forecast.all)
   }
 
-}  # End of looping through TemplateCore
+}  # End of looping through template data
 
 # check the elapsed time
 end.time <- Sys.time()
@@ -115,8 +102,87 @@ write.csv(forecast.all,
 forecast.all <- read.csv(file = paste0(filename.out, 
                                        ".csv"), 
                          stringsAsFactors=FALSE)    
- 
-  
+
+
+# Aggregate for n-day forecast summaries ------------------------------------
+
+# Aggregate the 7-day forecasted total precipitation 
+# (column "x" in the data frame created below)
+day7.forecast <- aggregate(forecast.all$precipitation.amount, 
+            by=list(locationid=forecast.all$locationid), 
+            FUN=sum)
+
+# find min/max dates within forecast 
+max.date <- max(forecast.all$date)
+min.date <- min(forecast.all$date)
+target.date <- as.Date(min.date)+4
+
+# filter forecast data to only contain "today" + 3 days
+day3.forecast <- filter(forecast.all, 
+                        forecast.all$date < target.date) 
+
+day3.forecast <- aggregate(day3.forecast$precipitation.amount, 
+                       by=list(locationid=day3.forecast$locationid), 
+                       FUN=sum)
+
+# Add shapewkt to full.7day.forecast and map it    
+map.forecast.day7 <- merge(day7.forecast, template.place)
+map.forecast.day3 <- merge(day3.forecast, template.place)
+
+
+# Mapping the forecast summary ----------------------------------------------------
+
+# specify lat/long coordinates. 38 and 2 are good for CLIMARK, zoom 7
+lon.x = 38  # center of get_map longitude 
+lat.y = 2.5   # center of get_map latitude 
+base.map.climark = get_map(location = c(lon = lon.x, lat = lat.y), 
+                           zoom = 7, 
+                           color = "bw")
+
+# display map of CLIMARK region
+gg.map <- ggmap(base.map.climark)
+gg.map
+
+# ggmap method - specify which forecast (4-day or 7-day in this case) to map
+ggmap.df <- map.forecast.4 
+
+# convert to data table. clip sum precip values
+# greater than 299 to 300. convert back to data frame. 
+dt2 <- as.data.table(ggmap.df)
+dt2[,aPre := ggmap.df$x]
+dt2[aPre > 299, aPre := 300]
+ggmap.df <- as.data.frame(dt2)
+
+polygon.df = as.tibble(wicket::wkt_coords(ggmap.df$shapewkt))
+polygon.df$aPre <- ggmap.df$aPre[polygon.df$object]  
+climark.map.title.precip <- paste0("Precipitation ", 
+                         min.date,"  ", 
+                         max.date) 
+climark.map.var.precip <- "Precipitation (mm)"
+
+map.precip = ggmap(base.map.climark) +
+  geom_polygon( aes( x = lng, 
+                     y = lat, 
+                     group = object, 
+                     fill = aPre),
+                data = polygon.df, 
+                alpha = 0.7) +
+  scale_fill_gradient2(breaks = seq(0,300, by = 50), 
+                       low = "red", 
+                       mid = "green",
+                       high = "blue", 
+                       midpoint = 150, name=climark.map.var.precip ) +
+  ggtitle(climark.map.title.precip)
+
+map.precip
+
+# save map to file 
+ggsave(filename = paste0(climark.map.title.precip,
+                         ".png"), 
+       map.precip, 
+       width = 6.02, 
+       height = 3.38, 
+       units = "in")
   
     
     
@@ -145,11 +211,11 @@ forecast.all <- read.csv(file = paste0(filename.out,
     only4dayf <- aggregate(day3forecast$forePre, by=list(locationid=day3forecast$locationid), FUN=sum)
 #add shapewkt to full7dayf and map it    
     #subsetdata <- full7dayf[which(full7dayf$locationid %in% TemplateCore$locationid),] 
-    MapForecast7 <- merge(full7dayf, TemplateCore)
-    MapForecast4 <- merge(only4dayf, TemplateCore)
+    map.forecast.7 <- merge(full7dayf, TemplateCore)
+    map.forecast.4 <- merge(only4dayf, TemplateCore)
     
     
-    AOItemplate <- st_as_sf(MapForecast7, wkt = "WKT")
+    AOItemplate <- st_as_sf(map.forecast.7, wkt = "WKT")
     st_crs(AOItemplate) = 4326   #this is the code to WGS84 projection
 maptitle <- "Precip Forecast 8-14 April Odisha"
     tmap_mode("view")
@@ -159,7 +225,7 @@ maptitle <- "Precip Forecast 8-14 April Odisha"
               breaks = seq(from = 0, to = 150, by = 20),
               id = "locationid", title = maptitle)
  
-       AOItemplate <- st_as_sf(MapForecast4, wkt = "WKT")
+       AOItemplate <- st_as_sf(map.forecast.4, wkt = "WKT")
     st_crs(AOItemplate) = 4326   #this is the code to WGS84 projection
     maptitle <- "Precip (mm) Forecast 8-11 April Odisha"
     tmap_mode("view")
